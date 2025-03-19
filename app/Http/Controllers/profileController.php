57a88abf1c969e79ff3ecdfb696c571554b11a3f
Mabdoul <iisimo29@gmail.com>
2025-03-19 16:38:33 +0000

    Refactor profileController and create view; enhance form validation and error handling

--- app/Http/Controllers/profileController.php
+++ app/Http/Controllers/profileController.php
@@ -8,47 +8,53 @@ use Illuminate\Http\Request;
 
 class profileController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         // dd(Profile::all());
         $profiles = Profile::paginate(9);
-        return view('profile.profiles',compact('profiles'));
+        return view('profile.profiles', compact('profiles'));
     }
     // public function show(Request $request){
     //     $id=(int)$request->id;
     //     $profile = Profile::findOrfail($id);
     //     return view('profile.show',compact('profile'));
     // }
-    public function show(Profile $profile){
-        return view('profile.show',compact('profile'));
+    public function show(Profile $profile)
+    {
+        return view('profile.show', compact('profile'));
     }
-    public function create(){
-        
+    public function create()
+    {
+
         return view('profile.create');
     }
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         // $name = $request->name;
         // $email = $request->email;
         // $password = $request->password;
         // $bio = $request->bio;
         //validate
         $request->validate([
-            'name'=>'required',
-            
-            ]);
-            //insert
-            // Profile::create([
-            //     'name'=>$name,
-            //     'email'=>$email,
-            //     'password'=>$password,
-            //     'bio'=>$bio,
-            // ]) ;
-            Profile::create($request->post());
-            //Redirections
+            'name' => 'required|between:3,25',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8|confirmed',
+            'bio' => 'nullable|string',
+        ]);
+        //insert
+        // Profile::create([
+        //     'name'=>$name,
+        //     'email'=>$email,
+        //     'password'=>$password,
+        //     'bio'=>$bio,
+        // ]) ;
+        Profile::create($request->post());
+        //Redirections
 
-            //redirect('b url b7al /home')
-            //redirect()->route('....') wla hadi to_route('...') --->hadi a7sen tari9a
-            //redirect()->action(...)
-            //back()->withInput() retour en arriere
-            return redirect()->route('profiles.index')->with('success', 'Your account created successfully');
-        }
+        //redirect('b url b7al /home')
+        //redirect()->route('....') wla hadi to_route('...') --->hadi a7sen tari9a
+        //redirect()->action(...)
+        //back()->withInput() retour en arriere
+        return redirect()->route('profiles.index')->with('success', 'Your account created successfully');
+    }
 }
--- resources/views/profile/create.blade.php
+++ resources/views/profile/create.blade.php
@@ -1,28 +1,50 @@
 <x-master title="Mon Profile" >
     <h3>Ajouter Profile</h3>
- 
+    @if ($errors->any())
+    <x-alert type="danger">
+      <ul>
+      @foreach ($errors->all() as $error )
+      <li>{{ $error }}</li>
+      
+        
+      @endforeach
+      </ul>
+    </x-alert>
+   
+        
+    @endif
     <form action="{{ route('store') }}" method="POST">
       @csrf
       <div class="form-group">
-        <label >full Name</label>
-        <input type="text" name="name" id="" class="form-control" >
-      
+          <label for="name">Full Name</label>
+          <input type="text" name="name" id="name" class="form-control" value="{{ old('name') }}">
+          @error('name')
+              <div class="text-danger">{{ $message }}</div>
+          @enderror
       </div>
       <div class="form-group">
-        <label >email</label>
-        <input type="text" name="email" id="" class="form-control" >
-      
+          <label for="email">Email</label>
+          <input type="text" name="email" id="email" class="form-control" value="{{ old('email') }}">
+          @error('email')
+              <div class="text-danger">{{ $message }}</div>
+          @enderror
       </div>
       <div class="form-group">
-        <label >password</label>
-        <input type="password" name="password" id="" class="form-control" >
-   
+          <label for="password">Password</label>
+          <input type="password" name="password" id="password" class="form-control">
+          @error('password')
+              <div class="text-danger">{{ $message }}</div>
+          @enderror
       </div>
       <div class="form-group">
-        <label >bio</label>
-        <textarea name="bio" class="form-control" cols="30" rows="10"></textarea>
+          <label for="bio">Bio</label>
+          <textarea name="bio" id="bio" class="form-control">{{ old('bio') }}</textarea>
+          @error('bio')
+              <div class="text-danger">{{ $message }}</div>
+          @enderror
       </div>
       <button type="submit" class="btn btn-primary">Ajouter</button>
-    </form>
+  </form>
+  
 
 </x-master>
