1377dcbe8a077af1722d790cb3a9d0e73cd4cf60
Mabdoul <iisimo29@gmail.com>
2025-03-21 16:32:11 +0000

    Add LoginController and login view; implement login form and navigation link

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,15 +1,18 @@
 @once<ul class="nav nav-tabs">
-    <li class="nav-item">
-      <a class="nav-link active" aria-current="page" href={{ route('homepage') }}>Home</a>
-    </li>
-    
-    <li class="nav-item">
-      <a class="nav-link" href={{ route('profiles.index') }}>Tous les Profile</a>
-    </li>
-    {{-- <li class="nav-item">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href={{ route('homepage') }}>Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="{{ route('login.show') }}">Se connecter</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href={{ route('profiles.index') }}>Tous les Profile</a>
+        </li>
+        {{-- <li class="nav-item">
       <a class="nav-link" href={{ route('settings.index') }}>Settings</a>
     </li> --}}
-    <li class="nav-item">
-      <a class="nav-link" href={{ route('profiles.create') }}>Ajouter profile</a>
-    </li>
-  </ul>@endonce
+        <li class="nav-item">
+            <a class="nav-link" href={{ route('profiles.create') }}>Ajouter profile</a>
+        </li>
+</ul>@endonce
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SettingsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('homepage');
+Route::get('/login', [LoginController::class, 'show'])->name('login.show');
+Route::post('/login', [LoginController::class, 'login'])->name('login');
 
 Route::get('/profiles', [ProfileController::class, 'index'])->name('profiles.index');
 
